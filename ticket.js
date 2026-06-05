be1ec542d9d62daa1668a5f3e466940e9e2700bf
shahde <sh240505sh@gmail.com>
2026-06-05 13:14:58 +0300

    added ticket price checker

---
+++ ticket.js
@@ -0,0 +1,13 @@
+let age = prompt("Please enter your age:");
+if (age <5){
+    console.log("free");
+}
+else if (age >=5 && age <=17){
+    console.log("$10");
+}
+else if (age >=18 && age <=64){
+    console.log("$20");
+}
+else (age >=65){
+    console.log("$12");
+}
